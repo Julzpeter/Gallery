0e7ad846582d990a756307f71998570a9d5cddbc
Juliet <chepngetichjuliet@gmail.com>
2019-07-19 10:41:29 +0300

    adds styles and images in media

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.welcome,name='home-page'),
+    url('^$',views.welcome,name='home-page'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^category/(\w+)', views.get_category, name='get_category'),
-    url(r'^location/(\w+)', views.get_location, name='get_location'),
+    url(r'^category/', views.get_category, name='get_category'),
+    url(r'^location/', views.get_location, name='get_location'),
 
  ]
 if settings.DEBUG:
