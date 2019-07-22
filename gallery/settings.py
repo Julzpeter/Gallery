3d69339afca5cddc4f3a5090b12f621b2c521707
Juliet <chepngetichjuliet@gmail.com>
2019-07-22 11:13:24 +0300

    corrects the syntax

--- gallery/settings.py
+++ gallery/settings.py
@@ -71,21 +71,21 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 # Development
-# if config('MODE')=="dev":
-#        DATABASES = {
-#        'default': {
-#            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#            'NAME': config('DB_NAME'),
-#            'USER': config('DB_USER'),
-#            'PASSWORD': config('DB_PASSWORD'),
-#            'HOST': config('DB_HOST'),
-#            'PORT': '',
-#        }
+if config('MODE')=="dev":
+       DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
 
-#    }
-# # production
-# else:
-DATABASES = {
+   }
+# production
+else:
+    DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
